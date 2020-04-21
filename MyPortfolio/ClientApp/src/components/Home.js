f12680b52a4229d4219216b47b5a909363c0d740
Jack Fagan <jack.fagan@damdigital.com>
2020-04-21 16:07:09 +0100

    further homepage styling changes made to match design

--- MyPortfolio/ClientApp/src/components/Home.js
+++ MyPortfolio/ClientApp/src/components/Home.js
@@ -3,6 +3,9 @@ import { Col, Row } from 'react-bootstrap';
 import TravelImage from '../media/img/travel.jpg';
 import CactusImage from '../media/img/cactus.jpg';
 import BuildingImage from '../media/img/building.jpg';
+import AppleImage from '../media/img/apple.jpg';
+import PlantImage from '../media/img/plant.jpg';
+import FoodImage from '../media/img/food.jpg';
 
 export class Home extends Component {
     displayName = Home.name
@@ -11,7 +14,8 @@ export class Home extends Component {
         return (
             <div className="homepage-container">
                 <div className="homepage-text">
-                    <h1>Jack Fagan</h1>
+                    <h1>Blog.</h1>
+                    <h2>Jack Fagan</h2>
                 </div>
                 <div className="homepage-articles">
                     <Row>
@@ -30,13 +34,13 @@ export class Home extends Component {
                         <Col className="homepage-half" sm={6}>
                             <Row>
                                 <Col sm={6}>
-                                    <img className="portrait-image" src={CactusImage} alt="Travel Article" />
+                                    <img className="portrait-image" src={FoodImage} alt="Travel Article" />
                                 </Col>
                                 <Col sm={6}>
-                                    <img className="portrait-image" src={BuildingImage} alt="Travel Article" />
+                                    <img className="portrait-image" src={PlantImage} alt="Travel Article" />
                                 </Col>
                             </Row>
-                            <img className="landscape-image" src={TravelImage} alt="Travel Article" />
+                            <img className="landscape-image" src={AppleImage} alt="Travel Article" />
                         </Col>
                     </Row>
                 </div>
--- MyPortfolio/ClientApp/src/components/NavMenu.js
+++ MyPortfolio/ClientApp/src/components/NavMenu.js
@@ -5,16 +5,22 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './_nav-menu.scss';
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
+    displayName = NavMenu.name
 
-  render() {
-      return (
-        <div className="navbar">
-              <a href="https://github.com/jackfagan"><img class="github" src="https://cdn.iconscout.com/icon/free/png-256/github-160-734866.png"/></a>
-              <a  href="https://twitter.com/jackfagan01"><img src="https://webstockreview.net/images/twitter-icon-png-6.png" /></a>
-              <a href="https://www.linkedin.com/in/jack-fagan-266930162/"><img src="https://img.icons8.com/ios/500/linkedin.png" /></a>
-              <hr />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="navbar">
+                <div>
+                    <a>Home</a>
+                    <a>All Articles</a>
+                </div>
+                <div>
+                    <a href="https://github.com/jackfagan"><img className="github" src="https://cdn.iconscout.com/icon/free/png-256/github-160-734866.png" /></a>
+                    <a href="https://twitter.com/jackfagan01"><img src="https://webstockreview.net/images/twitter-icon-png-6.png" /></a>
+                    <a href="https://www.linkedin.com/in/jack-fagan-266930162/"><img src="https://img.icons8.com/ios/500/linkedin.png" /></a>
+                    <hr />
+                </div>
+            </div>
+        );
+    }
 }
